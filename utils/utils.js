9749e2e9baf224fbb082e1435b6f526005b25685
unknown <d.kashmensky@gmail.com>
2020-05-14 12:55:08 +0300

    Remove checkUsername function

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,3 @@
 module.exports.checkEmail = (users, email) => {
   return users.find((item) => item.email === email);
 };
-
-module.exports.checkUsername = (users, username) => {
-  return users.find((item) => item.username === username);
-};
