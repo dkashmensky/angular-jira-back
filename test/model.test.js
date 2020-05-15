4031353e0375de451549a5c7e1b15459a13bf1fc
unknown <d.kashmensky@gmail.com>
2020-05-15 18:34:17 +0300

    Add mongoose models test

--- test/model.test.js
+++ test/model.test.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 const UserModel = require('../models/user.model');
+const TaskModel = require('../models/task.model');
 const user = {
   name: 'User',
   email: 'user@example.com',
   password: '123456',
   color: '255, 255, 255',
+};
+const task = {
+  title: 'Task',
+  type: 'Bug',
+  priority: 'Major',
+  status: 'To Do',
+  resolution: 'Unresolved',
+  description: 'Stuff',
+  persons: {
+    created: '5ebe5d416f384617a92c7579',
+    assigned: '5ebe5d416f384617a92c7579',
+  },
+  dates: {
+    created: 1589555589452,
+    updated: 1589555589452,
+    due: 1589555589452,
+    resolved: 1589555589452,
+  },
 };
 
 describe('Model testing', () => {
@@ -46,11 +65,11 @@ describe('Model testing', () => {
       }
 
       expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
-      expect(error.errors.email).toBeDefined();
       done();
     });
 
     test('fields not defined in schema should be undefined', async (done) => {
+      // eslint does not support object spread?
       const validUser = new UserModel({...user, ...{fullname: 'John'}});
       const savedUser = await validUser.save();
       expect(savedUser.name).toBeDefined();
@@ -60,8 +79,109 @@ describe('Model testing', () => {
   });
 
   describe('Task model', () => {
-    test('should create and save task', () => {
-      // TODO Task model tests
+    test('should create and save task', async (done) => {
+      const validTask = new TaskModel(task);
+      const savedTask = await validTask.save();
+      expect(savedTask._id).toBeDefined;
+      expect(savedTask.title).toBe(task.title);
+      expect(savedTask.type).toBe(task.type);
+      expect(savedTask.priority).toBe(task.priority);
+      expect(savedTask.status).toBe(task.status);
+      expect(savedTask.resolution).toBe(task.resolution);
+      expect(savedTask.description).toBe(task.description);
+      expect(savedTask.persons.created).toBe(task.persons.created);
+      expect(savedTask.persons.assigned).toBe(task.persons.assigned);
+      expect(savedTask.dates.created).toBe(task.dates.created);
+      expect(savedTask.dates.updated).toBe(task.dates.updated);
+      expect(savedTask.dates.resolved).toBe(task.dates.resolved);
+      expect(savedTask.dates.due).toBe(task.dates.due);
+      done();
+    });
+
+    test('should give an error if required fields are empty', async (done) => {
+      const invalidTask = new TaskModel({
+        title: 'Task',
+      });
+
+      let error;
+      try {
+        const savedTask = await invalidTask.save();
+        error = savedTask;
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      done();
+    });
+
+    test('fields not defined in schema should be undefined', async (done) => {
+      // eslint does not support object spread?
+      const validTask = new TaskModel({...task, ...{deadline: 123}});
+      const savedTask = await validTask.save();
+      expect(savedTask.title).toBeDefined();
+      expect(savedTask.deadline).toBeUndefined();
+      done();
+    })
+
+    test('should give an error if type has unallowed value', async (done) => {
+      const invalidTask = new UserModel({...task, ...{type: 'Unknown'}});
+
+      let error;
+      try {
+        const savedTask = await invalidTask.save();
+        error = savedTask;
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      done();
+    });
+
+    test('should give an error if priority has unallowed value', async (done) => {
+      const invalidTask = new UserModel({...task, ...{priority: 'Unknown'}});
+
+      let error;
+      try {
+        const savedTask = await invalidTask.save();
+        error = savedTask;
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      done();
+    });
+
+    test('should give an error if status has unallowed value', async (done) => {
+      const invalidTask = new UserModel({...task, ...{status: 'Unknown'}});
+
+      let error;
+      try {
+        const savedTask = await invalidTask.save();
+        error = savedTask;
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      done();
+    });
+
+    test('should give an error if resolution has unallowed value', async (done) => {
+      const invalidTask = new UserModel({...task, ...{resolution: 'Unknown'}});
+
+      let error;
+      try {
+        const savedTask = await invalidTask.save();
+        error = savedTask;
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+      done();
     });
   });
 
